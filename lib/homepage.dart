0d4d232e628f84af163f046b460f83e6caf553cb
didou0310 <104097523+didou0310@users.noreply.github.com>
2023-07-10 16:50:47 +0800

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,6 +9,7 @@ import 'comps/widgets.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -22,6 +23,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final firestore = FirebaseFirestore.instance;
   bool open = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,21 +33,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Chat App'),
         elevation: 0,
         centerTitle: true,
-        // actions: [
-        //   Padding(
-        //     padding: const EdgeInsets.only(right: 10.0),
-        //     child: IconButton(
-        //         onPressed: () {
-        //           setState(() {
-        //             open == true ? open = false : open = true;
-        //           });
-        //         },
-        //         icon: Icon(
-        //           open == true ? Icons.close_rounded : Icons.search_rounded,
-        //           size: 30,
-        //         )),
-        //   )
-        // ],
       ),
       drawer: ChatWidgets.drawer(context),
       body: SafeArea(
@@ -55,65 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Container(
-                //   margin: const EdgeInsets.all(0),
-                //   child: Container(
-                //     color: Colors.indigo.shade400,
-                //     padding: const EdgeInsets.all(8),
-                //     height: 160,
-                //     child: Column(
-                //       crossAxisAlignment: CrossAxisAlignment.start,
-                //       children: [
-                //         const Spacer(),
-                //         Padding(
-                //           padding: const EdgeInsets.symmetric(
-                //               horizontal: 8.0, vertical: 10),
-                //           child: Text(
-                //             'Recent Users',
-                //             style: Styles.h1(),
-                //           ),
-                //         ),
-                //         Container(
-                //           margin: const EdgeInsets.symmetric(vertical: 10),
-                //           height: 80,
-                //           child: StreamBuilder(
-                //             stream:  firestore.collection('Rooms').snapshots(),
-                //             builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                //               List data = !snapshot.hasData ? [] : snapshot.data!.docs.where((element) => element['users'].toString().contains(FirebaseAuth.instance.currentUser!.uid)).toList();
-                //               return ListView.builder(
-                //                 scrollDirection: Axis.horizontal,
-                //                 itemCount: data.length,
-                //                 itemBuilder: (context, i) {
-                //                   List users = data[i]['users'];
-                //                   var friend = users.where((element) => element != FirebaseAuth.instance.currentUser!.uid);
-                //                   var user = friend.isNotEmpty ? friend.first : users .where((element) => element == FirebaseAuth.instance .currentUser!.uid).first;
-                //                   return  FutureBuilder(
-                //                     future: firestore.collection('Users').doc(user).get(),
-                //                     builder: (context, AsyncSnapshot snap) {
-                //                       return !snap.hasData? Container(): ChatWidgets.circleProfile(onTap: (){
-                //                         Navigator.of(context)
-                //                             .push(
-                //                           MaterialPageRoute(
-                //                             builder: (context) {
-                //                               return ChatPage(
-                //                                 id: user,
-                //                                 name: snap.data['name'],
-                //                               );
-                //                             },
-                //                           ),
-                //                         );
-                //                       },name:  snap.data['name']);
-                //                     }
-                //                   );
-                //                 },
-                //               );
-                //             }
-                //           ),
-                //         ),
-                //       ],
-                //     ),
-                //   ),
-                // ),
                 Expanded(
                   child: Container(
                     margin: const EdgeInsets.only(top: 10),
@@ -138,36 +66,62 @@ class _MyHomePageState extends State<MyHomePage> {
                                     firestore.collection('Rooms').snapshots(),
                                 builder: (context,
                                     AsyncSnapshot<QuerySnapshot> snapshot) {
-                                  List data = !snapshot.hasData ? [] : snapshot.data!.docs.where((element) => element['users'].toString().contains(FirebaseAuth.instance.currentUser!.uid)).toList();
+                                  List data = !snapshot.hasData
+                                      ? []
+                                      : snapshot.data!.docs
+                                          .where((element) => element['users']
+                                              .toString()
+                                              .contains(FirebaseAuth.instance
+                                                  .currentUser!.uid))
+                                          .toList();
                                   return ListView.builder(
                                     itemCount: data.length,
                                     itemBuilder: (context, i) {
                                       List users = data[i]['users'];
-                                      var friend = users.where((element) => element != FirebaseAuth.instance.currentUser!.uid);
-                                      var user = friend.isNotEmpty ? friend.first : users .where((element) => element == FirebaseAuth.instance .currentUser!.uid).first;
+                                      var friend = users.where(
+                                          (element) =>
+                                              element !=
+                                              FirebaseAuth
+                                                  .instance.currentUser!.uid);
+                                      var user = friend.isNotEmpty
+                                          ? friend.first
+                                          : users.where((element) =>
+                                              element ==
+                                              FirebaseAuth
+                                                  .instance.currentUser!.uid)
+                                              .first;
                                       return FutureBuilder(
-                                        future: firestore.collection('Users').doc(user).get(),
-                                        builder: (context,AsyncSnapshot snap) {
-                                          return !snap.hasData? Container(): ChatWidgets.card(
-                                            title: snap.data['name'],
-                                            subtitle:data[i]['last_message'],
-                                            time: DateFormat('hh:mm a').format(data[i]['last_message_time'].toDate()),
-                                            onTap: () {
-                                              Navigator.of(context)
-                                                  .push(
-                                                MaterialPageRoute(
-                                                  builder: (context) {
-                                                    return ChatPage(
-                                                      id: user,
-                                                      name: snap.data['name'],
-                                                    );
-                                                  },
-                                                ),
-                                              );
-                                            },
-                                          );
-                                        }
-                                      );
+                                          future: firestore
+                                              .collection('Users')
+                                              .doc(user)
+                                              .get(),
+                                          builder: (context, AsyncSnapshot snap) {
+                                            return !snap.hasData
+                                                ? Container()
+                                                : ChatWidgets.card(
+                                                    title: snap.data['name'],
+                                                    subtitle: data[i]
+                                                        ['last_message'],
+                                                    time: DateFormat('hh:mm a')
+                                                        .format(data[i][
+                                                                'last_message_time']
+                                                            .toDate()),
+                                                    onTap: () {
+                                                      Navigator.of(context)
+                                                          .push(
+                                                        MaterialPageRoute(
+                                                          builder: (context) {
+                                                            return ChatPage(
+                                                              id: user,
+                                                              name: snap.data[
+                                                                  'name'],
+                                                            );
+                                                          },
+                                                        ),
+                                                      );
+                                                    },
+                                                  );
+                                          });
                                     },
                                   );
                                 }),
@@ -182,6 +136,36 @@ class _MyHomePageState extends State<MyHomePage> {
             ChatWidgets.searchBar(open)
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AnnouncementPage()),
+          );
+        },
+        child: Icon(
+          IconData(0xee78, fontFamily: 'MaterialIcons'),
+        ),
+        backgroundColor: Colors.indigo,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+    );
+  }
+}
+
+class AnnouncementPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Announcement Page'),
+      ),
+      body: Center(
+        child: Text('This is the announcement page'),
       ),
     );
   }
